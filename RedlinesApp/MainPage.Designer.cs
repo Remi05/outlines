1a38c63f8b05ebaf65927582f1415ff05a703f0a
Remi Pelletier <remipelletier5@gmail.com>
2021-03-23 00:23:28 -0700

    Added app logic

--- RedlinesApp/MainPage.Designer.cs
+++ RedlinesApp/MainPage.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainPage";
+            this.Text = "redlines";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainPage_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainPage_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainPage_MouseMove);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- RedlinesApp/MainPage.cs
+++ RedlinesApp/MainPage.cs
@@ -7,16 +7,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Redlines;
 
 namespace RedlinesApp
 {
     public partial class MainPage : Form
     {
+        private RedlinesService RedlinesService { get; set; } = new RedlinesService();
+
+        private Color DistanceOutlinesColor { get; set; } = Color.Red;
+        private Color SelectedElementOutlineColor { get; set; } = Color.Blue;
+        private Color TargetElementOutlineColor { get; set; } = Color.Green;
+
         public MainPage()
         {
             InitializeComponent();
             BackColor = Color.Magenta;
             TransparencyKey = Color.Magenta;
+        }
+
+        private void MainPage_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point screenPoint = PointToScreen(e.Location);
+            RedlinesService.OnCursorMove(Helpers.DrawingPointToWindowsPoint(screenPoint));
+        }
+
+        private void MainPage_MouseDown(object sender, MouseEventArgs e)
+        {
+            Point screenPoint = PointToScreen(e.Location);
+            RedlinesService.OnCursorDown(Helpers.DrawingPointToWindowsPoint(screenPoint));
+        }
+
+        private void MainPage_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var distanceOutline in RedlinesService.DistanceOutlines)
+            {
+                DrawDistanceOutline(e.Graphics, distanceOutline, DistanceOutlinesColor);
+            }
+
+            DrawElementOutline(e.Graphics, RedlinesService.SelectedElementProperties, SelectedElementOutlineColor);
+            DrawElementOutline(e.Graphics, RedlinesService.TargetElementProperties, TargetElementOutlineColor);
+        }
+
+        private void DrawDistanceOutline(Graphics graphics, DistanceOutline distanceOutline, Color color)
+        {
+            if (distanceOutline.Distance == 0)
+            {
+                return;
+            }
+
+            Point screenStartPoint = Helpers.WindowsPointToDrawingPoint(distanceOutline.StartPoint);
+            Point screenEndPoint = Helpers.WindowsPointToDrawingPoint(distanceOutline.EndPoint);
+            graphics.DrawLine(new Pen(color, 2), PointToClient(screenStartPoint), PointToClient(screenEndPoint));
+        }
+
+        private void DrawElementOutline(Graphics graphics, ElementProperties elementProperties, Color color)
+        {
+            if (elementProperties == null)
+            {
+                return;
+            }
+            Rectangle rectangle = Helpers.WindowsRectToDrawingRect(elementProperties.BoundingRect);
+            rectangle.Location = PointToClient(rectangle.Location);
+            graphics.DrawRectangle(new Pen(color, 2), rectangle);
         }
     }
 }
