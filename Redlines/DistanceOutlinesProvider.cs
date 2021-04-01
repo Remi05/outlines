bbf645b69f1fe39245532b1dfa3d093571da0369
Remi Pelletier <remipelletier5@gmail.com>
2021-04-01 00:00:00 -0700

    Split up GetDistanceOutlines fuctions

--- Redlines/DistanceOutlinesProvider.cs
+++ Redlines/DistanceOutlinesProvider.cs
@@ -19,106 +19,128 @@ namespace Redlines
 
             if (isSelectedContained || isTargetContained)
             {
-                Rect containedRect = isSelectedContained ? selectedElement.BoundingRect : targetElement.BoundingRect;
                 Rect containingRect = isSelectedContained ? targetElement.BoundingRect : selectedElement.BoundingRect;
-
-                Vector containedDiag = Point.Subtract(containedRect.BottomRight, containedRect.TopLeft);
-                Point containedCenter = Point.Add(containedRect.TopLeft, containedDiag / 2);
-                Point containedTopCenter = new Point(containedCenter.X, containedRect.Top);
-                Point containedBottomCenter = new Point(containedCenter.X, containedRect.Bottom);
-                Point containedLeftCenter = new Point(containedRect.Left, containedCenter.Y);
-                Point containedRightCenter = new Point(containedRect.Right, containedCenter.Y);
-
-                var topToTopOutline = new DistanceOutline(containedTopCenter, new Point(containedTopCenter.X, containingRect.Top));
-                var bottomToBottomOutline = new DistanceOutline(containedBottomCenter, new Point(containedBottomCenter.X, containingRect.Bottom));
-                var leftToLeftOutline = new DistanceOutline(containedLeftCenter, new Point(containingRect.Left, containedLeftCenter.Y));
-                var rightToRightOutline = new DistanceOutline(containedRightCenter, new Point(containingRect.Right, containedRightCenter.Y));
-
-                distanceOutlines.Add(topToTopOutline);
-                distanceOutlines.Add(bottomToBottomOutline);
-                distanceOutlines.Add(leftToLeftOutline);
-                distanceOutlines.Add(rightToRightOutline);
+                Rect containedRect = isSelectedContained ? selectedElement.BoundingRect : targetElement.BoundingRect;
+                distanceOutlines.AddRange(GetContainedDistanceOutlines(containingRect, containedRect));
             }
             else
             {
-                Vector selectedDiag = Point.Subtract(selectedElement.BoundingRect.BottomRight, selectedElement.BoundingRect.TopLeft);
-                Point selectedCenter = Point.Add(selectedElement.BoundingRect.TopLeft, selectedDiag / 2);
 
-                // Vertical
-                if (selectedElement.BoundingRect.Top > targetElement.BoundingRect.Bottom)
+                Vector selectedElementDiag = Point.Subtract(selectedElement.BoundingRect.BottomRight, selectedElement.BoundingRect.TopLeft);
+                Point selectedElementCenter = Point.Add(selectedElement.BoundingRect.TopLeft, selectedElementDiag / 2);
+
+                distanceOutlines.AddRange(GetVerticalOffsetDistanceOutlines(selectedElement, targetElement, selectedElementCenter));
+                distanceOutlines.AddRange(GetHorizontalOffsetDistanceOutlines(selectedElement, targetElement, selectedElementCenter));
+            }
+
+            return distanceOutlines;
+        }
+
+        private List<DistanceOutline> GetContainedDistanceOutlines(Rect containingRect, Rect containedRect)
+        {
+            Vector containedDiag = Point.Subtract(containedRect.BottomRight, containedRect.TopLeft);
+            Point containedCenter = Point.Add(containedRect.TopLeft, containedDiag / 2);
+            Point containedTopCenter = new Point(containedCenter.X, containedRect.Top);
+            Point containedBottomCenter = new Point(containedCenter.X, containedRect.Bottom);
+            Point containedLeftCenter = new Point(containedRect.Left, containedCenter.Y);
+            Point containedRightCenter = new Point(containedRect.Right, containedCenter.Y);
+
+            var topToTopOutline = new DistanceOutline(containedTopCenter, new Point(containedTopCenter.X, containingRect.Top));
+            var bottomToBottomOutline = new DistanceOutline(containedBottomCenter, new Point(containedBottomCenter.X, containingRect.Bottom));
+            var leftToLeftOutline = new DistanceOutline(containedLeftCenter, new Point(containingRect.Left, containedLeftCenter.Y));
+            var rightToRightOutline = new DistanceOutline(containedRightCenter, new Point(containingRect.Right, containedRightCenter.Y));
+
+            return new List<DistanceOutline>() 
+            { 
+                topToTopOutline,
+                bottomToBottomOutline,
+                leftToLeftOutline,
+                rightToRightOutline
+            };
+        }
+
+        private List<DistanceOutline> GetVerticalOffsetDistanceOutlines(ElementProperties selectedElement, ElementProperties targetElement, Point selectedElementCenter)
+        {
+            List<DistanceOutline> distanceOutlines = new List<DistanceOutline>();
+
+            if (selectedElement.BoundingRect.Top > targetElement.BoundingRect.Bottom)
+            {
+                // Top to Bottom
+                var selectedTopCenter = new Point(selectedElementCenter.X, selectedElement.BoundingRect.Top);
+                var lineEnd = new Point(selectedElementCenter.X, targetElement.BoundingRect.Bottom);
+                var topToBottomOutline = new DistanceOutline(selectedTopCenter, lineEnd);
+                distanceOutlines.Add(topToBottomOutline);
+
+                if (lineEnd.X < targetElement.BoundingRect.Left)
                 {
-                    // Top to Bottom
-                    var selectedTopCenter = new Point(selectedCenter.X, selectedElement.BoundingRect.Top);
-                    var lineEnd = new Point(selectedCenter.X, targetElement.BoundingRect.Bottom);
-                    var topToBottomOutline = new DistanceOutline(selectedTopCenter, lineEnd);
-                    distanceOutlines.Add(topToBottomOutline);
-
-                    if (lineEnd.X < targetElement.BoundingRect.Left)
-                    {
-                        distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.BottomLeft, true));
-                    }
-                    else if (lineEnd.X > targetElement.BoundingRect.Right)
-                    {
-                        distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.BottomRight, true));
-                    }
+                    distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.BottomLeft, true));
                 }
-                else if (selectedElement.BoundingRect.Bottom < targetElement.BoundingRect.Top)
+                else if (lineEnd.X > targetElement.BoundingRect.Right)
                 {
-                    // Bottom to Top
-                    var selectedBottomCenter = new Point(selectedCenter.X, selectedElement.BoundingRect.Bottom);
-                    var lineEnd = new Point(selectedCenter.X, targetElement.BoundingRect.Top);
-                    var bottomToTopOutline = new DistanceOutline(selectedBottomCenter, lineEnd);
-                    distanceOutlines.Add(bottomToTopOutline);
-
-                    if (lineEnd.X < targetElement.BoundingRect.Left)
-                    {
-                        distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.TopLeft, true));
-                    }
-                    else if (lineEnd.X > targetElement.BoundingRect.Right)
-                    {
-                        distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.TopRight, true));
-                    }
+                    distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.BottomRight, true));
                 }
+            }
+            else if (selectedElement.BoundingRect.Bottom < targetElement.BoundingRect.Top)
+            {
+                // Bottom to Top
+                var selectedBottomCenter = new Point(selectedElementCenter.X, selectedElement.BoundingRect.Bottom);
+                var lineEnd = new Point(selectedElementCenter.X, targetElement.BoundingRect.Top);
+                var bottomToTopOutline = new DistanceOutline(selectedBottomCenter, lineEnd);
+                distanceOutlines.Add(bottomToTopOutline);
 
-                // Horizontal
-                if (selectedElement.BoundingRect.Left > targetElement.BoundingRect.Right)
+                if (lineEnd.X < targetElement.BoundingRect.Left)
                 {
-                    // Left to Right
-                    var selectedLeftCenter = new Point(selectedElement.BoundingRect.Left, selectedCenter.Y);
-                    var lineEnd = new Point(targetElement.BoundingRect.Right, selectedCenter.Y);
-                    var leftToRightOutline = new DistanceOutline(selectedLeftCenter, lineEnd);
-                    distanceOutlines.Add(leftToRightOutline);
-
-                    if (lineEnd.Y < targetElement.BoundingRect.Top)
-                    {
-                        distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.TopRight, true));
-                    }
-                    else if (lineEnd.Y > targetElement.BoundingRect.Bottom)
-                    {
-                        distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.BottomRight, true));
-                    }
+                    distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.TopLeft, true));
                 }
-                else if (selectedElement.BoundingRect.Right < targetElement.BoundingRect.Left)
+                else if (lineEnd.X > targetElement.BoundingRect.Right)
                 {
-                    // Right to Left
-                    var selectedRightCenter = new Point(selectedElement.BoundingRect.Right, selectedCenter.Y);
-                    var lineEnd = new Point(targetElement.BoundingRect.Left, selectedCenter.Y);
-                    var rightToLeftOutline = new DistanceOutline(selectedRightCenter, new Point(targetElement.BoundingRect.Left, selectedCenter.Y));
-                    distanceOutlines.Add(rightToLeftOutline);
-
-                    if (lineEnd.Y < targetElement.BoundingRect.Top)
-                    {
-                        distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.TopLeft, true));
-                    }
-                    else if (lineEnd.Y > targetElement.BoundingRect.Bottom)
-                    {
-                        distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.BottomLeft, true));
-                    }
+                    distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.TopRight, true));
                 }
             }
 
             return distanceOutlines;
         }
 
+        private List<DistanceOutline> GetHorizontalOffsetDistanceOutlines(ElementProperties selectedElement, ElementProperties targetElement, Point selectedElementCenter)
+        {
+            List<DistanceOutline> distanceOutlines = new List<DistanceOutline>();
+
+            if (selectedElement.BoundingRect.Left > targetElement.BoundingRect.Right)
+            {
+                // Left to Right
+                var selectedLeftCenter = new Point(selectedElement.BoundingRect.Left, selectedElementCenter.Y);
+                var lineEnd = new Point(targetElement.BoundingRect.Right, selectedElementCenter.Y);
+                var leftToRightOutline = new DistanceOutline(selectedLeftCenter, lineEnd);
+                distanceOutlines.Add(leftToRightOutline);
+
+                if (lineEnd.Y < targetElement.BoundingRect.Top)
+                {
+                    distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.TopRight, true));
+                }
+                else if (lineEnd.Y > targetElement.BoundingRect.Bottom)
+                {
+                    distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.BottomRight, true));
+                }
+            }
+            else if (selectedElement.BoundingRect.Right < targetElement.BoundingRect.Left)
+            {
+                // Right to Left
+                var selectedRightCenter = new Point(selectedElement.BoundingRect.Right, selectedElementCenter.Y);
+                var lineEnd = new Point(targetElement.BoundingRect.Left, selectedElementCenter.Y);
+                var rightToLeftOutline = new DistanceOutline(selectedRightCenter, new Point(targetElement.BoundingRect.Left, selectedElementCenter.Y));
+                distanceOutlines.Add(rightToLeftOutline);
+
+                if (lineEnd.Y < targetElement.BoundingRect.Top)
+                {
+                    distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.TopLeft, true));
+                }
+                else if (lineEnd.Y > targetElement.BoundingRect.Bottom)
+                {
+                    distanceOutlines.Add(new DistanceOutline(lineEnd, targetElement.BoundingRect.BottomLeft, true));
+                }
+            }
+
+            return distanceOutlines;
+        }
     }
 }
