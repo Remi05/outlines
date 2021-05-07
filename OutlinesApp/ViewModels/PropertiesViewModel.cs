004efd3f9c459a856007a923e4b3948bb82b6f5e
Remi Pelletier <remipelletier5@gmail.com>
2021-05-07 03:10:45 -0700

    Updated PropertiesViewModel to notify of property changes

--- OutlinesApp/ViewModels/PropertiesViewModel.cs
+++ OutlinesApp/ViewModels/PropertiesViewModel.cs
@@ -1,17 +1,48 @@
 ﻿using Outlines;
+using System.ComponentModel;
 
 namespace OutlinesApp.ViewModels
 {
-    public class PropertiesViewModel
+    public class PropertiesViewModel : INotifyPropertyChanged
     {
         protected IOutlinesService OutlinesService { get; set; }
-        public ElementProperties ElementProperties { get; protected set; }
-        public TextProperties TextProperties { get; protected set; }
-    
+
+        private ElementProperties elementProperties;
+        public ElementProperties ElementProperties
+        {
+            get => elementProperties;
+            private set
+            {
+                if (value != elementProperties)
+                {
+                    elementProperties = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ElementProperties)));
+                }
+            }
+        }
+
+        private TextProperties textProperties;
+        public TextProperties TextProperties
+        {
+            get => textProperties;
+            private set
+            {
+                if (value != textProperties)
+                {
+                    textProperties = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TextProperties)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public PropertiesViewModel(IOutlinesService outlinesService)
         {
             OutlinesService = outlinesService;
             OutlinesService.SelectedElementChanged += OnSelectedElementChanged;
+            ElementProperties = OutlinesService.SelectedElementProperties;
+            TextProperties = OutlinesService.SelectedTextProperties;
         }
 
         protected void OnSelectedElementChanged()
