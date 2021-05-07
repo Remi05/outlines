2b8388820fe4335d4e4fe0f7c84f24900b324278
Remi Pelletier <remipelletier5@gmail.com>
2021-05-07 03:13:19 -0700

    Registered services in MainWindow

--- OutlinesApp/MainWindow.xaml.cs
+++ OutlinesApp/MainWindow.xaml.cs
@@ -1,28 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
+using OutlinesApp.ViewModels;
+using Outlines;
 
 namespace OutlinesApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+
+            IOutlinesService outlinesService = new OutlinesService();
+            IGlobalInputListener globalInputListener = new GlobalInputListener();
+            InspectorViewModel inspectorViewModel = new InspectorViewModel(outlinesService, globalInputListener);
+            PropertiesViewModel propertiesViewModel = new PropertiesViewModel(outlinesService);
+
+            var serviceContainer = ServiceContainer.Instance;
+            serviceContainer.AddService(typeof(IOutlinesService), outlinesService);
+            serviceContainer.AddService(typeof(IGlobalInputListener), globalInputListener);
+            serviceContainer.AddService(typeof(InspectorViewModel), inspectorViewModel);
+            serviceContainer.AddService(typeof(PropertiesViewModel), propertiesViewModel);
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var globalInputListener = ServiceContainer.Instance.GetService<IGlobalInputListener>();
+            globalInputListener?.RegisterToInputEvents();
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            var globalInputListener = ServiceContainer.Instance.GetService<IGlobalInputListener>();
+            globalInputListener?.UnregisterFromInputEvents();
         }
     }
 }
