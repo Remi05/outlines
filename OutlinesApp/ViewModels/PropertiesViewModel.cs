cec1fb3c3b837a6fa180f32e262b08be8d7eccf4
Remi Pelletier <remipelletier5@gmail.com>
2021-05-06 21:59:09 -0700

    Added PropertiesPanel and PropertiesViewModel

---
+++ OutlinesApp/ViewModels/PropertiesViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Outlines;
+
+namespace OutlinesApp.ViewModels
+{
+    public class PropertiesViewModel
+    {
+        protected IOutlinesService OutlinesService { get; set; }
+        public ElementProperties ElementProperties { get; protected set; }
+        public TextProperties TextProperties { get; protected set; }
+    
+        public PropertiesViewModel(IOutlinesService outlinesService)
+        {
+            OutlinesService = outlinesService;
+            OutlinesService.SelectedElementChanged += OnSelectedElementChanged;
+        }
+
+        protected void OnSelectedElementChanged()
+        {
+            ElementProperties = OutlinesService.SelectedElementProperties;
+            TextProperties = OutlinesService.SelectedTextProperties;
+        }
+    }
+}
---
+++ OutlinesApp/Views/PropertiesPanel.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using OutlinesApp.ViewModels;
+
+namespace OutlinesApp.Views
+{
+    public partial class PropertiesPanel : UserControl
+    {
+        public PropertiesViewModel ViewModel { get; private set; }
+
+        public PropertiesPanel()
+        {
+            InitializeComponent();
+            DataContext = ViewModel;
+        }
+    }
+}
