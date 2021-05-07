484a85edb3b5c58e24ead1e9a983213bfb2dd874
Remi Pelletier <remipelletier5@gmail.com>
2021-05-07 03:14:52 -0700

    Added OutlinesLayer

---
+++ OutlinesApp/Views/OutlinesLayer.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using OutlinesApp.ViewModels;
+using Outlines;
+
+namespace OutlinesApp.Views
+{
+    public partial class OutlinesLayer : UserControl
+    {
+        public OutlinesLayer()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DataContext = ServiceContainer.Instance.GetService<InspectorViewModel>();
+        }
+    }
+}
