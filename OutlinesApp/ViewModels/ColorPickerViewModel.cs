92363ff05bf76e9baab3715da7b4b5edc073a7a7
Remi Pelletier <remipelletier5@gmail.com>
2021-05-08 02:55:15 -0700

    Added ColorPickerViewModel

---
+++ OutlinesApp/ViewModels/ColorPickerViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using Outlines;
+
+namespace OutlinesApp.ViewModels
+{
+    public class ColorPickerViewModel : INotifyPropertyChanged
+    {
+        private IColorPickerService ColorPickerService { get; set; }
+        private IGlobalInputListener GlobalInputListener { get; set; }
+
+        private Color pickedColor;
+        public Color PickedColor
+        {
+            get => pickedColor;
+            private set
+            {
+                if (value != pickedColor)
+                {
+                    pickedColor = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PickedColor)));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PickedColorRbg)));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PickedColorHex)));
+                }
+            }
+        }
+
+        public string PickedColorRbg => PickedColor == null ? "" : $"({PickedColor.R},{PickedColor.G},{PickedColor.B})";
+
+        public string PickedColorHex => PickedColor == null ? "" : $"#{PickedColor.R.ToString("X2")}{PickedColor.G.ToString("X2")}{PickedColor.B.ToString("X2")}";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ColorPickerViewModel(IColorPickerService colorPickerService, IGlobalInputListener globalInputListener)
+        {
+            ColorPickerService = colorPickerService;
+            GlobalInputListener = globalInputListener;
+            GlobalInputListener.MouseMoved += OnMouseMove;
+        }
+
+        private void OnMouseMove(Point cursorPos)
+        {
+            PickedColor = ColorPickerService.GetColorAt(cursorPos);
+        }
+    }
+}
