0c4fd9d55ae9c3cc78096f8335d806c1c662c5f0
Remi Pelletier <remipelletier5@gmail.com>
2021-05-11 18:17:29 -0700

    Added DistanceViewModel

---
+++ OutlinesApp/ViewModels/DistanceViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using Outlines;
+using OutlinesApp.Services;
+
+namespace OutlinesApp.ViewModels
+{
+    public class DistanceViewModel
+    {
+        private const int TextContainerRectWidth = 75;
+        private const int TextContainerRectHeight = 25;
+
+        private DistanceOutline DistanceOutline { get; set; }
+        private IScreenHelper ScreenHelper { get; set; }
+
+        public string DistanceText => $"{DistanceOutline.Distance}";
+
+        public bool IsDashedLine => DistanceOutline.IsAlignmentLine;
+        public bool IsTextVisible => !DistanceOutline.IsAlignmentLine && DistanceOutline.Distance != 0;
+        public bool IsVertical => DistanceOutline.IsVertical;
+
+        public Point StartPoint { get; private set; }
+        public Point EndPoint { get; private set; }
+
+        public Rect TextContainerRect { get; private set; }
+
+        public DistanceViewModel(DistanceOutline distanceOutline, IScreenHelper screenHelper)
+        {
+            DistanceOutline = distanceOutline;
+            ScreenHelper = screenHelper;
+            StartPoint = ScreenHelper.PointFromScreen(distanceOutline.StartPoint);
+            EndPoint = ScreenHelper.PointFromScreen(distanceOutline.EndPoint);
+            InitializeTextContainerRect();
+        } 
+
+        private void InitializeTextContainerRect()
+        {
+            var localMidPoint = ScreenHelper.PointFromScreen(DistanceOutline.MidPoint);
+            var textContainerTopLeft = DistanceOutline.IsVertical
+                                     ? new Point(localMidPoint.X, localMidPoint.Y - TextContainerRectHeight / 2)
+                                     : new Point(localMidPoint.X - TextContainerRectWidth / 2, localMidPoint.Y);
+            TextContainerRect = new Rect(textContainerTopLeft, new Size(TextContainerRectWidth, TextContainerRectHeight));
+        }
+    }
+}
