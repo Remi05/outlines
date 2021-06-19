daa08594321e654eecf33003856af1e0c59f32d1
Remi Pelletier <remipelletier5@gmail.com>
2021-06-19 00:45:56 -0700

    Added RectangleDetectionService draft

---
+++ Outlines.ImageProcessing/RectangleDetectionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Outlines.ImageProcessing
+{
+    public class RectangleDetectionService
+    {
+        public List<DetectedRectangle> DetectRectangles(List<Line> lines)
+        {
+            var rectangles = new List<DetectedRectangle>();
+            return rectangles;
+        }
+    }
+}
