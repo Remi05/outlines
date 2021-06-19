a08c5dd489173bfbaab805a759d1e43f117f222d
Remi Pelletier <remipelletier5@gmail.com>
2021-06-19 00:26:43 -0700

    Added Line class

---
+++ Outlines.ImageProcessing/Line.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace Outlines.ImageProcessing
+{
+    public class Line
+    {
+        public Point Start { get; private set; }
+        public Point End { get; private set; }
+
+        public Line(Point start, Point end)
+        {
+            if (start.X > end.X)
+            {
+                throw new ArgumentException($"{nameof(start)}.X should always be smaller or equal to {nameof(end)}.X");
+            }
+            if (start.Y > end.Y)
+            {
+                throw new ArgumentException($"{nameof(start)}.Y should always be smaller or equal to {nameof(end)}.Y");
+            }
+            Start = start;
+            End = end;
+        }
+    }
+}
