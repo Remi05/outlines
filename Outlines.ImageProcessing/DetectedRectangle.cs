8689417cb795cda80b692bae5c946fe188d12490
Remi Pelletier <remipelletier5@gmail.com>
2021-06-19 00:27:50 -0700

    Added DetectedRectangle class

---
+++ Outlines.ImageProcessing/DetectedRectangle.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace Outlines.ImageProcessing
+{
+    public class DetectedRectangle
+    {
+        public Rectangle Rect { get; private set; }
+        public Line Left { get; private set; }
+        public Line Top { get; private set; }
+        public Line Right { get; private set; }
+        public Line Bottom { get; private set; }
+
+        public DetectedRectangle(Rectangle rect, Line left, Line top, Line right, Line bottom)
+        {
+            Rect = rect;
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+    }
+}
