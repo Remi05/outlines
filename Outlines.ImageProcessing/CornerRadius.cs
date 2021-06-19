d1722c2788617e36e59eabd35bf1fc7008f49712
Remi Pelletier <remipelletier5@gmail.com>
2021-06-19 00:26:28 -0700

    Added CornerRadius class

---
+++ Outlines.ImageProcessing/CornerRadius.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Outlines.ImageProcessing
+{
+    public class CornerRadius
+    {
+        public int TopLeft { get; private set; }
+        public int TopRight { get; private set; }
+        public int BottomRight { get; private set; }
+        public int BottomLeft { get; private set; }
+
+        public CornerRadius(int topLeft, int topRight, int bottomRight, int bottomLeft)
+        {
+            TopLeft = topLeft;
+            TopRight = topRight;
+            BottomRight = bottomRight;
+            BottomLeft = bottomLeft;
+        }
+    }
+}
