5f5c9e05e0490ef66de8ec66f2f3094f190d07c8
Remi Pelletier <remipelletier5@gmail.com>
2021-06-19 00:46:08 -0700

    Added EdgeDetectionFilterDraft

---
+++ Outlines.ImageProcessing/EdgeDetectionFilter.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Outlines.ImageProcessing
+{
+    public class EdgeDetectionFilter
+    {
+        public Bitmap Apply(Bitmap image)
+        {
+            var imageWithFilter = new Bitmap(image);
+            return imageWithFilter;
+        }
+    }
+}
