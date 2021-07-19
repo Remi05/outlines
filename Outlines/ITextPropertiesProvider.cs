455aab582351c686a5a8b528dea5fb1260e2bac1
Remi Pelletier <remipelletier5@gmail.com>
2021-07-19 00:58:48 -0700

    Extracted ITextPropertiesProvider interface

---
+++ Outlines/ITextPropertiesProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Automation;
+
+namespace Outlines
+{
+    public interface ITextPropertiesProvider
+    {
+        TextProperties GetTextProperties(AutomationElement element);
+    }
+}
