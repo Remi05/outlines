fa47a057f4aa8aabec1e2f5cf6c4ad64302006da
Remi Pelletier <remipelletier5@gmail.com>
2021-07-19 01:01:45 -0700

    Added UiTreeService and UiTreeNode

---
+++ Outlines/UiTreeNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Outlines
+{
+    public class UiTreeNode
+    {
+        public ElementProperties ElementProperties { get; private set; }
+        public List<UiTreeNode> Children { get; private set; }
+
+        public UiTreeNode(ElementProperties elementProperties, List<UiTreeNode> children = null)
+        {
+            if (elementProperties == null)
+            {
+                throw new ArgumentNullException(nameof(elementProperties));
+            }
+            ElementProperties = elementProperties;
+            Children = children ?? new List<UiTreeNode>();
+        }
+    }
+}
---
+++ Outlines/UiTreeService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Automation;
+
+namespace Outlines
+{
+    public delegate void RootNodeChangedEventHandler();
+
+    public class UiTreeService : IUiTreeService
+    {
+        private const int DefaultMaxTreeDepth = 4;
+
+        private IElementPropertiesProvider ElementPropertiesProvider { get; set; }
+        private IOutlinesService OutlinesService { get; set; }
+
+        private UiTreeNode rootNode = null;
+        public UiTreeNode RootNode
+        {
+            get => rootNode;
+            private set
+            {
+                if (value != rootNode)
+                {
+                    rootNode = value;
+                    RootNodeChanged?.Invoke();
+                }
+            }
+        }
+
+        public event RootNodeChangedEventHandler RootNodeChanged;
+
+        public UiTreeService(IElementPropertiesProvider elementPropertiesProvider, IOutlinesService outlinesService)
+        {
+            if (elementPropertiesProvider == null || outlinesService == null)
+            {
+                throw new ArgumentNullException(elementPropertiesProvider == null ? nameof(elementPropertiesProvider) : nameof(outlinesService));
+            }
+            ElementPropertiesProvider = elementPropertiesProvider;
+            OutlinesService = outlinesService;
+            OutlinesService.SelectedElementChanged += UpdateRootNode;
+            UpdateRootNode();
+        }
+
+        private void UpdateRootNode()
+        {
+            var selectedElementProperties = OutlinesService.SelectedElementProperties;
+            if (selectedElementProperties != null)
+            {
+                RootNode = GetTreeNode(selectedElementProperties);
+            }
+            else
+            {
+                var rootElementProperties = ElementPropertiesProvider.GetElementProperties(AutomationElement.RootElement);
+                RootNode = GetTreeNode(rootElementProperties);
+            }
+        }
+
+        private UiTreeNode GetTreeNode(ElementProperties rootElementProperties, int remainingDepth = DefaultMaxTreeDepth)
+        {
+            if (rootElementProperties == null)
+            {
+                return null;
+            }
+            try
+            {
+                var childrenElements = rootElementProperties.Element.FindAll(TreeScope.Children, Condition.TrueCondition);
+                var childrenNodes = new List<UiTreeNode>();
+                if (remainingDepth > 0)
+                {
+                    foreach (var child in childrenElements)
+                    {
+                        ElementProperties childElementProperties = ElementPropertiesProvider.GetElementProperties(child as AutomationElement);
+                        UiTreeNode childNode = GetTreeNode(childElementProperties, remainingDepth - 1);
+                        if (childNode != null)
+                        {
+                            childrenNodes.Add(childNode);
+                        }
+                    }
+                }
+                return new UiTreeNode(rootElementProperties, childrenNodes);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
