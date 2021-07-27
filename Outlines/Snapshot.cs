81579fa1d99eba423ce4c62765de22d128402f51
Remi Pelletier <remipelletier5@gmail.com>
2021-07-26 18:05:19 -0700

    Added Snapshot class

---
+++ Outlines/Snapshot.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Outlines
+{
+    public class Snapshot
+    {
+        public UiTreeNode UiTree { get; set; }
+        public string ScreenshotFilePath { get; set; }
+
+        [JsonIgnore]
+        private Image Screenshot { get; set; }
+
+        public Snapshot(UiTreeNode uiTreeRoot, string screenshotFilePath)
+        {
+            UiTree = uiTreeRoot;
+            ScreenshotFilePath = screenshotFilePath;
+        }
+
+        public Snapshot(UiTreeNode uiTreeRoot, Image screenshot)
+        {
+            UiTree = uiTreeRoot;
+            Screenshot = screenshot;
+        }
+
+        public Image GetScreenshot()
+        {
+            if (Screenshot == null)
+            {
+                if (ScreenshotFilePath == null || !File.Exists(ScreenshotFilePath))
+                {
+                    return null;
+                }
+                Screenshot = Image.FromFile(ScreenshotFilePath);
+            }
+            return Screenshot;
+        }
+    }
+}
