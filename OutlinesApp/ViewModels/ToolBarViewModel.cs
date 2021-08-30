fac8335a9622b0274f960c9d44f511fe4b138e06
Remi Pelletier <remipelletier5@gmail.com>
2021-08-30 11:55:14 -0400

    Hide screenshot button

--- OutlinesApp/ViewModels/ToolBarViewModel.cs
+++ OutlinesApp/ViewModels/ToolBarViewModel.cs
@@ -14,8 +14,11 @@ namespace OutlinesApp.ViewModels
         private IScreenshotService ScreenshotService { get; set; }
         private ISnapshotService SnapshotService { get; set; }
         private IFolderConfig FolderConfig { get; set; }
-        public InspectorViewModel InspectorViewModel { get; set; }   
-        
+        public InspectorViewModel InspectorViewModel { get; set; }
+
+        public bool IsScreenshotButtonVisible => ScreenshotService != null;
+        public bool IsSnapshotButtonVisible => SnapshotService != null;
+
         public RelayCommand<object> CloseAppCommand { get; private set; }
         public RelayCommand<object> GetHelpCommand { get; private set; }
         public RelayCommand<object> GiveFeedbackCommand { get; private set; }
@@ -25,11 +28,9 @@ namespace OutlinesApp.ViewModels
 
         public ToolBarViewModel(IOutlinesService outlinesService, IScreenshotService screenshotService, ISnapshotService snapshotService, IFolderConfig folderConfig, InspectorViewModel inspectorViewModel)
         {
-            if (outlinesService == null || screenshotService == null || snapshotService == null || folderConfig == null || inspectorViewModel == null)
+            if (outlinesService == null || folderConfig == null || inspectorViewModel == null)
             {
-                throw new ArgumentNullException(outlinesService == null ? nameof(outlinesService) 
-                                              : screenshotService == null ? nameof(screenshotService) 
-                                              : snapshotService == null ? nameof(snapshotService) 
+                throw new ArgumentNullException(outlinesService == null ? nameof(outlinesService)
                                               : folderConfig == null ? nameof(folderConfig)
                                               : nameof(inspectorViewModel));
             }
@@ -43,8 +44,8 @@ namespace OutlinesApp.ViewModels
             GetHelpCommand = new RelayCommand<object>(_ => GetHelp());
             GiveFeedbackCommand = new RelayCommand<object>(_ => GiveFeedback());
             ShowMoreInfoCommand = new RelayCommand<object>(_ => ShowMoreInfo());
-            TakeScreenshotCommand = new RelayCommand<object>(_ => TakeScreenshot(), __ => OutlinesService.SelectedElementProperties != null);
-            TakeSnapshotCommand = new RelayCommand<object>(_ => TakeSnapshot(), __ => OutlinesService.SelectedElementProperties != null);
+            TakeScreenshotCommand = new RelayCommand<object>(_ => TakeScreenshot());
+            TakeSnapshotCommand = new RelayCommand<object>(_ => TakeSnapshot());
         }
 
         private void GetHelp()
@@ -64,21 +65,38 @@ namespace OutlinesApp.ViewModels
 
         private void TakeScreenshot()
         {
+            Image screenshot;
             if (OutlinesService.SelectedElementProperties != null)
             {
-                string fileName = $"Screenshot-{DateTime.Now.ToFileTime()}.png";
-                string filePath = Path.Combine(FolderConfig.GetScreenshotsFolderPath(), fileName);
-                Image screenshot = ScreenshotService.TakeScreenshot(OutlinesService.SelectedElementProperties);
-                screenshot.Save(filePath, ImageFormat.Png);
+                screenshot = ScreenshotService.TakeScreenshot(OutlinesService.SelectedElementProperties);
             }
+            else
+            {
+                var window = App.Current.MainWindow;
+                //var windowBounds = new Rectangle((int)window.Left, (int)window.Top, (int)window.Width, (int)window.Height);
+                var windowBounds = new Rectangle(0, 0, 1920, 1080);
+                screenshot = ScreenshotService.TakeScreenshot(windowBounds);
+            }
+
+            string fileName = $"Screenshot-{DateTime.Now.ToFileTime()}.png";
+            string filePath = Path.Combine(FolderConfig.GetScreenshotsFolderPath(), fileName);
+            screenshot.Save(filePath, ImageFormat.Png);
         }
+
         private void TakeSnapshot()
         {
+            Snapshot snapshot;
             if (OutlinesService.SelectedElementProperties != null)
             {
-                Snapshot snapshot = SnapshotService.TakeSnapshot(OutlinesService.SelectedElementProperties);
-                SnapshotService.SaveSnapshot(snapshot);
+                snapshot = SnapshotService.TakeSnapshot(OutlinesService.SelectedElementProperties);
             }
+            else
+            {
+                var window = App.Current.MainWindow;
+                var windowBounds = new Rectangle((int)window.Left, (int)window.Top, (int)window.Width, (int)window.Height);
+                snapshot = SnapshotService.TakeSnapshot(windowBounds);
+            }
+            SnapshotService.SaveSnapshot(snapshot);
         }
     }
 }
