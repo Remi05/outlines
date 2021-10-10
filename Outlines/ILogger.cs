39df346bb3c72ba8fd62fd1d474132e42fc8aabc
Remi Pelletier <remipelletier5@gmail.com>
2021-10-10 18:11:13 -0400

    Added ILogger interface

---
+++ Outlines/ILogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Outlines
+{
+    public enum LoggingLevel
+    {
+        Trace,
+        Debug,
+        Info,
+        Warn,
+        Error,
+        Fatal
+    }
+
+    public interface ILogger
+    {
+        void Log(LoggingLevel logginLevel, string message);
+    }
+}
