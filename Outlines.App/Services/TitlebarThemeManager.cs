47d16f40ddbc737e8e8ff1c84bca106c1e754eb1
Remi Pelletier <remipelletier5@gmail.com>
2021-10-20 20:35:17 -0700

    Added TitlebarThemeManager

---
+++ Outlines.App/Services/TitlebarThemeManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Outlines.App.Services
+{
+    public class TitlebarThemeManager
+    {
+        private ThemeManager ThemeManager { get; set; }
+        private TitlebarHelper TitlebarHelper { get; set; }
+        private IntPtr WindowToUpdate { get; set; }
+
+        public TitlebarThemeManager(ThemeManager themeManager, TitlebarHelper titlebarHelper, IntPtr windowToUpdate)
+        {
+            ThemeManager = themeManager;
+            TitlebarHelper = titlebarHelper;
+            WindowToUpdate = windowToUpdate;
+
+            UpdateTitlebarTheme();
+            ThemeManager.ThemeChanged += UpdateTitlebarTheme;
+        }
+
+        private void UpdateTitlebarTheme()
+        {
+            Color titlebarBackgroundColor = (Color)Application.Current.Resources["ThemeTitlebarBackgroundColor"];
+            TitlebarHelper.SetTitlebarBackgroundColor(WindowToUpdate, titlebarBackgroundColor);
+        }
+    }
+}
