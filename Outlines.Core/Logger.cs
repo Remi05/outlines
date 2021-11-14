878eb05fe02f58e96e43fc1c97b75b4fa52d429f
Remi Pelletier <remipelletier5@gmail.com>
2021-11-14 01:12:20 -0800

    Added basic logger

---
+++ Outlines.Core/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Outlines.Core
+{
+    public enum LoggingLevel
+    {
+        Trace,
+        Debug,
+        Info,
+        Warn,
+        Error,
+        Fatal
+    }
+
+    public static class Logger
+    {
+        private const string LogsDirectory = "Logs/";
+        private static StreamWriter Output { get; set; }
+
+        static Logger()
+        {
+            if (!Directory.Exists(LogsDirectory))
+            {
+                Directory.CreateDirectory(LogsDirectory);
+            }
+            string logFileName = $"Log-{DateTime.Now.ToFileTime()}.txt";
+            string logFilePath = Path.Combine(LogsDirectory, logFileName);
+            Output = new StreamWriter(File.Create(logFilePath), Encoding.UTF8);
+        }
+
+        public static void Log(LoggingLevel loggingLevel, string message)
+        {
+            Output.WriteLine($"[{loggingLevel}] {message}");
+        }
+    }
+}
