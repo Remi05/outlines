c46159b4649a8b610d192c17b351da916d29e984
Remi Pelletier <remipelletier5@gmail.com>
2021-12-28 03:24:41 -0500

    Added SingleWindowLiveInspector

---
+++ Outlines.App/Services/SingleWindowLiveInspector.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Outlines.App.Services
+{
+    public class SingleWindowLiveInspector : ILiveInspector
+    {
+        private LiveInspectorWindow LiveInspectorWindow { get; set; }
+
+        public void Show()
+        {
+            LiveInspectorWindow = new LiveInspectorWindow();
+            LiveInspectorWindow.Show();
+        }
+
+        public void Close()
+        {
+            LiveInspectorWindow?.Close();
+        }
+    }
+}
