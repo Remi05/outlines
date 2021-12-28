bba03f0fe4917030e17d2b82bbb8cadba35b2f22
Remi Pelletier <remipelletier5@gmail.com>
2021-12-28 03:22:22 -0500

    Added InspectorStateManager

---
+++ Outlines.App/Services/InspectorStateManager.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Outlines.App.Services
+{
+    public class InspectorStateManager : IInspectorStateManager
+    {
+        private bool isOverlayVisible = true;
+        public bool IsOverlayVisible
+        {
+            get => isOverlayVisible;
+            set
+            {
+                if (value != isOverlayVisible)
+                {
+                    isOverlayVisible = value;
+                    IsOverlayVisibleChanged?.Invoke(IsOverlayVisible);
+                }
+            }
+        }
+
+        private bool isPropertiesPanelVisible = true;
+        public bool IsPropertiesPanelVisible
+        {
+            get => isPropertiesPanelVisible;
+            set
+            {
+                if (value != isPropertiesPanelVisible)
+                {
+                    isPropertiesPanelVisible = value;
+                    IsPropertiesPanelVisibleChanged?.Invoke(IsPropertiesPanelVisible);
+                }
+            }
+        }
+
+        private bool isTreeViewVisible = true;
+        public bool IsTreeViewVisible
+        {
+            get => isTreeViewVisible;
+            set
+            {
+                if (value != isTreeViewVisible)
+                {
+                    isTreeViewVisible = value;
+                    IsTreeViewVisibleChanged?.Invoke(IsTreeViewVisible);
+                }
+            }
+        }
+
+        public event IsOverlayVisibleChangedHandler IsOverlayVisibleChanged;
+        public event IsPropertiesPanelVisibleChangedHandler IsPropertiesPanelVisibleChanged;
+        public event IsTreeViewVisibleChangedHandler IsTreeViewVisibleChanged;
+    }
+}
