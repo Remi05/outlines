c7b6a82ae5287d3c85411f8f7f1862f6f1cd7b0b
Remi Pelletier <remipelletier5@gmail.com>
2021-12-28 03:20:40 -0500

    Added OverlayWindow

---
+++ Outlines.App/Windows/OverlayWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Outlines.App
+{
+    public partial class OverlayWindow : Window
+    {
+        public OverlayWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
