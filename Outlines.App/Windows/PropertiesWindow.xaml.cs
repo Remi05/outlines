9494aac7b9e4d3d2636a281bda40f8fc34f26c6b
Remi Pelletier <remipelletier5@gmail.com>
2021-12-28 03:01:18 -0500

    Added PropertiesWindow

---
+++ Outlines.App/Windows/PropertiesWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Outlines.App
+{
+    public partial class PropertiesWindow : Window
+    {
+        public PropertiesWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnHandleMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
