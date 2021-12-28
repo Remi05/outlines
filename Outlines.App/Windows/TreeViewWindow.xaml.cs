5b2a3d86c7f8f2c5b71fe29c4b4027e3c222ec1e
Remi Pelletier <remipelletier5@gmail.com>
2021-12-28 03:16:13 -0500

    Added TreeViewWindow

---
+++ Outlines.App/Windows/TreeViewWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Outlines.App
+{
+    public partial class TreeViewWindow : Window
+    {
+        public TreeViewWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnHandleMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
