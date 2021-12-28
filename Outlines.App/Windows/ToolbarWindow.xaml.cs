15dbfcd14fc5573e64a0850fae1910350f9618f5
Remi Pelletier <remipelletier5@gmail.com>
2021-12-28 02:57:59 -0500

    Added ToolbarWindow

---
+++ Outlines.App/Windows/ToolbarWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Outlines.App
+{
+    public partial class ToolbarWindow : Window
+    {
+        public ToolbarWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnHandleMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
