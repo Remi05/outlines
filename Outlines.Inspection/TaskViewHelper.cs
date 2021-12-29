e79321f75f9495debfdf00048e34b61e45ac14b0
Remi Pelletier <remipelletier5@gmail.com>
2021-12-28 23:11:21 -0500

    Added TaskViewHelper

---
+++ Outlines.Inspection/TaskViewHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Outlines.Inspection
+{
+    public class TaskViewHelper
+    {
+        public void HideWindowFromTaskView(IntPtr hwnd)
+        {
+            int windowStyle = NativeWindowService.GetWindowLong(hwnd, NativeWindowService.WindowInfoIndices.GWL_EXSTYLE);
+            windowStyle |= (int)NativeWindowService.ExtendedWindowStyles.WS_EX_TOOLWINDOW;
+            NativeWindowService.SetWindowLong(hwnd, NativeWindowService.WindowInfoIndices.GWL_EXSTYLE, windowStyle);
+        }
+    }
+}
