50fc3fc01c0d14e4b45d3559b26214ccd09d5b3c
Remi Pelletier <remipelletier5@gmail.com>
2021-12-31 16:40:31 -0500

    Added IgnorableWindowsSource

---
+++ Outlines.App/Services/IgnorableWindowsSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Interop;
+using Outlines.Inspection;
+
+namespace Outlines.App.Services
+{
+    public class IgnorableWindowsSource : IIgnorableWindowsSource
+    {
+        protected ISet<IntPtr> WindowsToIgnore { get; set; } = new HashSet<IntPtr>();
+
+        public ISet<IntPtr> GetWindowsToIgnore() => WindowsToIgnore;
+
+        public void IgnoreWindow(Window window)
+        {
+            if (window != null)
+            {
+                IntPtr hwnd = new WindowInteropHelper(window).Handle;
+                WindowsToIgnore.Add(hwnd);
+            }
+        }
+    }
+}
