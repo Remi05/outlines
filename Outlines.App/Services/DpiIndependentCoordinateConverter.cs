796744329c43d86eccb8979ac10529eb5d9d1684
Remi Pelletier <remipelletier5@gmail.com>
2022-01-31 15:00:48 -0800

    Added DpiIndependentCoordinateConverter

---
+++ Outlines.App/Services/DpiIndependentCoordinateConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using Outlines.Core;
+
+namespace Outlines.App.Services
+{
+    // Coordinate converter to be used when the screen and window coordinates are expected to match 1:1 (eg. when no DPI scaling is performed).
+    public class DpiIndependentCoordinateConverter : ICoordinateConverter
+    {
+        public Point PointFromScreen(Point screenPoint) => screenPoint;
+        public Point PointToScreen(Point localPoint) => localPoint;
+        public Size SizeFromScreen(Size screenSize) => screenSize;
+        public Size SizeToScreen(Size localSize) => localSize;
+        public Rectangle RectFromScreen(Rectangle screenRect) => screenRect;
+        public Rectangle RectToScreen(Rectangle localRect) => localRect;
+    }
+}
