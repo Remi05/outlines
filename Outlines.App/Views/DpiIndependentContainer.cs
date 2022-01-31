555d460736b9fecef1a23f05d09da759a3096cae
Remi Pelletier <remipelletier5@gmail.com>
2022-01-31 14:49:06 -0800

    Added DpiIndependentContainer

---
+++ Outlines.App/Views/DpiIndependentContainer.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Outlines.App.Views
+{
+    public class DpiIndependentContainer : Decorator
+    {
+        public DpiIndependentContainer()
+        {
+            Loaded += (_, __) => OnLoaded();
+        }
+
+        private void OnLoaded()
+        {
+            Matrix transformToDevice = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice;
+            var inverseDpiTransform = new ScaleTransform(1.0 / transformToDevice.M11, 1.0 / transformToDevice.M22);
+            if (inverseDpiTransform.CanFreeze)
+            {
+                inverseDpiTransform.Freeze();
+            }
+            LayoutTransform = inverseDpiTransform;
+        }
+    }
+}
