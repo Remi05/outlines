988d735b41f3e2ccb4e29337ddb07c376eb5addc
Remi Pelletier <remipelletier5@gmail.com>
2022-02-04 17:06:12 -0800

    Added BackdropWindow

---
+++ Outlines.App/Windows/BackdropWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Outlines.App
+{
+    public partial class BackdropWindow : Window
+    {
+        public BackdropWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
