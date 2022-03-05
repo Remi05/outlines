90b7d11644eb29606a549dbe97398c451ed534e4
Remi Pelletier <remipelletier5@gmail.com>
2022-03-04 20:01:23 -0800

    Backup

---
+++ Outlines.App/Views/HighlightRectangle.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Outlines.App.Views
+{
+    public partial class HighlightRectangle : UserControl
+    {
+        public HighlightRectangle()
+        {
+            InitializeComponent();
+        }
+    }
+}
