473b8ec9e9041b6fe12363e70d543c57470561f5
Remi Pelletier <remipelletier5@gmail.com>
2022-09-20 16:51:50 -0700

    Added UIAutomationStructureChangedEventHandler

---
+++ Outlines.Inspection/UIAutomationEventHandlers.cs
@@ -0,0 +1,21 @@
+﻿using UIAutomationClient;
+
+namespace Outlines.Inspection
+{
+    internal delegate void StructureChangedHandler(IUIAutomationElement sender);
+
+    internal class UIAutomationStructureChangedEventHandler : IUIAutomationStructureChangedEventHandler
+    {
+        private StructureChangedHandler StructureChangedHandler { get; set; }
+
+        public UIAutomationStructureChangedEventHandler(StructureChangedHandler handler)
+        {
+            StructureChangedHandler = handler;
+        }
+
+        public void HandleStructureChangedEvent(IUIAutomationElement sender, StructureChangeType changeType, int[] runtimeId)
+        {
+            StructureChangedHandler?.Invoke(sender);
+        }
+    }
+}
