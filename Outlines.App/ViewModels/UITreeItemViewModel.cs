ad875a7a4842af6f3727334745c923b156e48e90
Remi Pelletier <remipelletier5@gmail.com>
2022-09-20 17:25:11 -0700

    Updated TreeView UI to lazy-load and dynamically update

--- Outlines.App/ViewModels/UITreeItemViewModel.cs
+++ Outlines.App/ViewModels/UITreeItemViewModel.cs
@@ -1,43 +1,81 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Threading;
 using Outlines.Core;
 
 namespace Outlines.App.ViewModels
 {
     public class UITreeItemViewModel : INotifyPropertyChanged
     {
-        public UITreeNode UITreeNode { get; private set; }
+        private Dispatcher Dispatcher { get; set; }
+        public IUITreeNode UITreeNode { get; private set; }
 
         public string ElementName
         {
             get
             {
+                if (UITreeNode == null)
+                {
+                    return "Loading...";
+                }
                 string name = UITreeNode.ElementProperties.Name;
                 return (string.IsNullOrWhiteSpace(name) ? "<unnamed>" : name) + $" - {UITreeNode.ElementProperties.ControlType}";
             }
         }
 
+        private bool shouldShowChildren = false;
+        public bool ShouldShowChildren
+        {
+            get => shouldShowChildren;
+            set
+            {
+                if (value != shouldShowChildren)
+                {
+                    shouldShowChildren = value;
+                    if (shouldShowChildren && !WereChildrenLoaded)
+                    {
+                        UpdateChildrenElements();
+                    }
+                }
+            }
+        }
+
+        private bool WereChildrenLoaded { get; set; } = false;
         public ObservableCollection<UITreeItemViewModel> ChildrenElements { get; private set; } = new ObservableCollection<UITreeItemViewModel>();
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public UITreeItemViewModel(UITreeNode uiTreeNode)
+        public UITreeItemViewModel(Dispatcher dispatcher, IUITreeNode uiTreeNode, bool showChildren = false)
         {
-            if (uiTreeNode == null)
+            Dispatcher = dispatcher;
+            if (uiTreeNode != null)
             {
-                throw new ArgumentNullException(nameof(uiTreeNode));
+                UITreeNode = uiTreeNode;
+                UITreeNode.ChildrenChanged += UpdateChildrenElements;
+                if (UITreeNode.HasChildren)
+                {
+                    // We ned to add a placeholder child so that users can expand the node
+                    // (at which point we'll lazy-load the children and remove the placeholder).
+                    ChildrenElements.Add(new UITreeItemViewModel(Dispatcher, null, false));
+                }
             }
-            UITreeNode = uiTreeNode;
-            UpdateChildrenElements();
+            ShouldShowChildren = showChildren;
         }
 
-        private void UpdateChildrenElements()
+        private async void UpdateChildrenElements()
         {
-            foreach (var child in UITreeNode.Children)
+            WereChildrenLoaded = true;
+            var childrenElements = UITreeNode.GetAndMonitorChildren();
+
+            Dispatcher.Invoke(() =>
             {
-                ChildrenElements.Add(new UITreeItemViewModel(child));
-            }
+                ChildrenElements.Clear();
+                foreach (var child in childrenElements)
+                {
+                    ChildrenElements.Add(new UITreeItemViewModel(Dispatcher, child));
+                }
+                ShouldShowChildren = true;
+            });
         }
     }
 }
