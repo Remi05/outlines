d056bc58fd531b3c1bbc595fe07d0b13cf13b725
Rémi Pelletier <remipelletier5@gmail.com>
2024-04-20 17:29:55 -0400

    Added ability to copy element properties on click

--- Outlines.App/Views/PropertiesPanel.xaml.cs
+++ Outlines.App/Views/PropertiesPanel.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using Outlines.App.Services;
 using Outlines.App.ViewModels;
 
@@ -8,12 +11,27 @@ namespace Outlines.App.Views
     {
         public PropertiesPanel()
         {
-            InitializeComponent();         
+            InitializeComponent();
         }
 
-        private void OnLoaded(object sender, System.Windows.RoutedEventArgs e)
+        private void OnLoaded(object sender, RoutedEventArgs e)
         {
             Root.DataContext = ServiceContainer.Instance.GetService<PropertiesViewModel>();
+        }
+
+        private void ElementProperty_OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is TextBlock textBlock)
+            {
+                Clipboard.SetText(textBlock.Text);
+
+                ToolTip toolTip = new();
+                toolTip.Content = "Copied!";
+                toolTip.IsOpen = true;
+
+                // Hide the ToolTip after 1s.
+                Task.Delay(1000).ContinueWith(_ => Dispatcher.Invoke(() => toolTip.IsOpen = false));
+            }
         }
     }
 }
